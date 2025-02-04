963d705ad5ecb094699c36bbfc7c8557df2173ff
Le Thanh Nguyen <u7594144@anu.edu.au>
2025-02-05 00:06:11 +0700

    added grid view sidebar item

--- src/app/_components/Table/TableSideItem.tsx
+++ src/app/_components/Table/TableSideItem.tsx
@@ -1,3 +1,4 @@
+import { fi } from '@faker-js/faker';
 import React from 'react';
 
 type TableSideItemProps = {
@@ -5,9 +6,10 @@ type TableSideItemProps = {
   name: string,
   color?: string
   d?: string
+  fillRule?: string
 }
 
-const TableSideItem = ({handleClick, name, color, d} : TableSideItemProps) => {
+const TableSideItem = ({handleClick, name, color, d, fillRule} : TableSideItemProps) => {
   return (
     <div role='button' onClick={handleClick}  className='w-full flex items-center justify-between p-2 rounded-md hover:bg-slate-200'>
       <div className='flex items-center gap-2'>
@@ -18,6 +20,7 @@ const TableSideItem = ({handleClick, name, color, d} : TableSideItemProps) => {
           viewBox="0 0 16 16"
           className="flex-none"
           fill={color}
+          fill-rule={fillRule}
           >
           <path fill-rule="non-zero" d={d} />
           </svg>)}
